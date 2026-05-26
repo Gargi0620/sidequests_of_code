c7e87cc3cf39bf9b91cdd2e2d961fa48824d494d
Gargi0620 <gargik75@gmail.com>
2026-05-27 02:44:42 +0530

    Added Valid-Anagram solution in java

---
+++ Strings/Valid-Anagram.java
@@ -0,0 +1,22 @@
+// Valid Anagram (leetcode #242)
+// Pattern: Frequency Count
+// level: Easy
+// Time Complexity : O(nlogn)
+//Space Complexity : O(n)
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
