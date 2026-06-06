07ce88ac87d799f2f586616f4344044dd03c15bf
Gargi Kulkarni <gargik75@gmail.com>
2026-06-06 23:06:34 +0530

    Proper solution for leetcode 283 - Move zeroes

---
+++ Two-Pointer/Move-Zeroes.java
@@ -0,0 +1,21 @@
+// Move Zeroes (leetcode #283)
+// Pattern: Two Pointer
+// level: Easy
+// Time Complexity : O(n)
+//Space Complexity : O(1)
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+        }        
+    }
+}
+
